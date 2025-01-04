378bd9e64524363041b574cafe22fce3f3424c78
atlantamimi <1monkeyduck@gmail.com>
2025-01-03 20:14:26 -0500

    Fixed code and verified output

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-Initial commit
-"Initial commit"
 print("Hello, World!")
-print("Learning GitHub!")  # Add this new line
-print("Hello, World!")
-print("Learning GitHub!")  # Add this new line
-print("Making a new change!")  # Add this NEW line
-print("Hello, World!")
-print("Learning GitHub!")  # Add this new line
-print("Making a new change!")  # Add this NEW line
+print("Learning GitHub")
+print("Making a new change!")

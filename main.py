7583a2760207eb08c122a91320f35d4907613cd5
atlantamimi <1monkeyduck@gmail.com>
2025-01-03 20:01:00 -0500

    Added third print statement

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ Initial commit
 "Initial commit"
 print("Hello, World!")
 print("Learning GitHub!")  # Add this new line
+print("Hello, World!")
+print("Learning GitHub!")  # Add this new line
+print("Making a new change!")  # Add this NEW line
+print("Hello, World!")
+print("Learning GitHub!")  # Add this new line
+print("Making a new change!")  # Add this NEW line
